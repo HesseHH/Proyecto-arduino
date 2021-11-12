82d819950a12215566e1cea9c9a21df1cbf19558
HesseHH <hesseh2121@gmail.com>
2021-11-12 19:42:04 -0300

    add Delete Update Search in DataActivity

--- app/src/main/java/com/example/proyectarduino/DataActivity.java
+++ app/src/main/java/com/example/proyectarduino/DataActivity.java
@@ -2,12 +2,18 @@ package com.example.proyectarduino;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.proyectarduino.helpers.Helper;
 
 public class DataActivity extends AppCompatActivity {
 
-    EditText email, pass;
+    private EditText email, name;
     ConnectionSQLiteHelper connection;
 
 
@@ -18,7 +24,53 @@ public class DataActivity extends AppCompatActivity {
 
         connection = new ConnectionSQLiteHelper(this, "bd_users", null, 1);
 
-        email = (EditText) findViewById(R.id.txtEmail);
-        pass = (EditText) findViewById(R.id.txtPassword);
+        email = (EditText) findViewById(R.id.txtEmailData);
+        name = (EditText) findViewById(R.id.txtNameData);
+    }
+
+    private void searchUser() {
+        SQLiteDatabase db = connection.getWritableDatabase();
+        String[] parameters = {email.getText().toString()};
+        String[] columns = {Helper.NOMBRE};
+
+        try {
+            Cursor cursor = db.query(
+                    Helper.TABLA_USUARIO,
+                    columns,
+                    Helper.CORREO + "=?",
+                    parameters,
+                    null,
+                    null,
+                    null);
+
+            cursor.moveToFirst();
+            name.setText(cursor.getString(0));
+            cursor.close();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(),"El correo electrónico no existe",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void updateUser() {
+        SQLiteDatabase db = connection.getWritableDatabase();
+        String[] parameters = {email.getText().toString()};
+
+        ContentValues values = new ContentValues();
+        values.put(Helper.NOMBRE, name.getText().toString());
+
+        db.update(Helper.TABLA_USUARIO, values, Helper.CORREO + "=?", parameters);
+        Toast.makeText(getApplicationContext(),"Usuario Actualizado",Toast.LENGTH_LONG).show();
+        db.close();
+    }
+
+    private void deleteUser() {
+        SQLiteDatabase db = connection.getWritableDatabase();
+        String[] parameters = {email.getText().toString()};
+
+        db.delete(Helper.TABLA_USUARIO, Helper.CORREO + "=?", parameters);
+        Toast.makeText(getApplicationContext(),"Usuario Actualizado",Toast.LENGTH_LONG).show();
+        db.close();
+        email.setText("");
+        name.setText("");
     }
 }
