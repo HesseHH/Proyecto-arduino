1f399a4f434c89a0f910fb35483498a109e289cc
HesseHH <hesseh2121@gmail.com>
2021-12-22 17:07:13 -0300

    Lights terminado

---
+++ app/src/main/java/com/example/proyectarduino/ConnectionBluetooth.java
@@ -0,0 +1,14 @@
+package com.example.proyectarduino;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConnectionBluetooth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connection_bluetooth);
+    }
+}
